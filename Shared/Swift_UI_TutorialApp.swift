ebc9189b178ec7fb779199750ab931d166046e23
lydiaanzures <lydiaanzures@gmail.com>
2022-01-27 20:37:05 -0500

    Initial Commit

---
+++ Shared/Swift_UI_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_TutorialApp.swift
+//  Shared
+//
+//  Created by Lydia Anzures on 1/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
